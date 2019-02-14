fe89fee8571b6b2a6a02dcf7b90f673d8e1015fa
Eric Schanet <eric.schanet@cern.ch>
2019-02-14 21:32:26 +0100

    Forgot resource factor

--- getJobStatsFlat.py
+++ getJobStatsFlat.py
@@ -78,7 +78,7 @@ for site, site_result in siteResourceStats.iteritems():
                     resource_factor = 8.0
                 else:
                     resource_factor = 1.0
-                value["resource_factor"] = resource_factor
+                
                 json_body = {   "measurement": "jobs",
                                 "tags": {
                                     "atlas_site": atlas_site,
@@ -91,7 +91,8 @@ for site, site_result in siteResourceStats.iteritems():
                                 },
                                 "time" : time,
                                 "fields" : {
-                                    "jobs" : value[job_status]
+                                    "jobs" : value[job_status],
+                                    "resource_factor" : resource_factor
                                 }
                             }
 
