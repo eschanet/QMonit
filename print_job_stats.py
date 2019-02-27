c5bbb987cb0046e8938f6a7141b34e9007cfefe9
Eric Schanet <eric.schanet@cern.ch>
2019-02-27 13:46:56 +0100

    Various modifications

--- print_job_stats.py
+++ print_job_stats.py
@@ -6,12 +6,18 @@ from pprint import pprint
 import json,sys
 import requests
 import cPickle as pickle
-from datetime import datetime
-
-# from influxdb import InfluxDBClient
+from datetime import datetime,timedelta
+import hashlib
+import ConfigParser
 import Client
 
-#-------------------------------------------------------------------------------
+config = ConfigParser.ConfigParser()
+config.read("config.cfg")
+
+password = config.get("credentials", "password")
+username = config.get("credentials", "username")
+database = config.get("credentials", "database")
+
 baseURL = 'http://pandaserver.cern.ch:25080/server/panda'
 
 url_cloudJobs  = baseURL + '/getJobStatistics'
@@ -27,52 +33,77 @@ with open('pandaqueue.json') as pandaqueue:
 
 with open('pandaresource.json') as pandaresource:
     panda_resources = json.load(pandaresource)
-#-------------------------------------------------------------------------------
+
 err, siteResourceStats = Client.getJobStatisticsPerSiteResource()
 
-# pprint(siteResourceStats0)
-
-
-# pprint(siteResourceStats0['TRIUMF_DOCKER_UCORE'])
+# client = InfluxDBClient('dbod-eschanet.cern.ch', 8080, username, password, "monit_jobs", True, False)
 
 points_list = []
+
+# Explicitly set timestamp in InfluxDB point. Avoids having multiple entries per 10 minute interval (can happen sometimes with acron)
+epoch = datetime.utcfromtimestamp(0)
+def unix_time_nanos(dt):
+    return (dt - epoch).total_seconds() * 1e9
+
+current_time = datetime.utcnow()
+current_time = current_time - timedelta(minutes=current_time.minute % 10,
+                             seconds=current_time.second,
+                             microseconds=current_time.microsecond)
+
+unix = int(unix_time_nanos(current_time))
 
 for site, site_result in siteResourceStats.iteritems():
 
     for core, value in site_result.iteritems():
-        current_time = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-        # print(site)
-        #
-        # pprint(value)
+        for job_status in value.keys():
 
-        if site in panda_resources:
-            queue = panda_resources[site]
+            # simple hack to protect against duplicate entries
+            # each site-core combination will have its unique **hash**
+            m = hashlib.md5()
+            m.update(site + core + job_status)
+            time = unix + int(str(int(m.hexdigest(), 16))[0:9])
 
-            atlas_site = panda_queues[queue]["atlas_site"]
-            type = panda_queues[queue]["type"]
-            cloud = panda_queues[queue]["cloud"]
-            state = panda_queues[queue]["state"]
+            if site in panda_resources:
+                queue = panda_resources[site]
 
-            json_body = {   "measurement": "jobs",
-                            "tags": {
-                                "atlas_site": atlas_site,
-                                "panda_queue" : site,
-                                "resource" : core,
-                                "type" : type,
-                                "cloud" : cloud,
-                                "state" : state
-                            },
-                            "time" : current_time,
-                            "fields" : value
-                        }
+                atlas_site = panda_queues[queue]["atlas_site"]
+                type = panda_queues[queue]["type"]
+                cloud = panda_queues[queue]["cloud"]
+                site_state = panda_queues[queue]["state"]
 
-            points_list.append(json_body)
-        else:
-            print("ERROR  -  Site %s not in panda resources"%site)
+                if "MCORE" in core:
+                    resource_factor = 8.0
+                else:
+                    resource_factor = 1.0
+                if job_status == "running":
+                    n_jobs = value[job_status]*int(resource_factor)
+                else:
+                    n_jobs = value[job_status]
+
+                json_body = {   "measurement": "jobs",
+                                "tags": {
+                                    "atlas_site": atlas_site,
+                                    "panda_queue" : site,
+                                    "resource" : core,
+                                    "type" : type,
+                                    "cloud" : cloud,
+                                    "site_state" : site_state,
+                                    "job_status" : job_status
+                                },
+                                "time" : time,
+                                "fields" : {
+                                    "jobs" : n_jobs,
+                                    "resource_factor" : resource_factor
+                                }
+                            }
+
+                points_list.append(json_body)
+
+            else:
+                print("ERROR  -  Site %s not in panda resources"%site)
+
+# client.write_points(points=points_list, time_precision="n")
 
 
-        # pprint(json_body)
-
-# print("Number of points to be uploaded")
-# pprint(points_list)
+print("Got number of points: {}".format(len(points_list)))
