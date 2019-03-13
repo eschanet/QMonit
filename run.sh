b05b5012dfe0bc8dd72eab590a3cece0794177c0
Eric Schanet <eric.schanet@cern.ch>
2019-03-13 17:39:10 +0100

    Adding this to run script

--- run.sh
+++ run.sh
@@ -11,6 +11,9 @@ cd /afs/cern.ch/user/e/eschanet/qualitask/
 #get updated queue info from AGIS
 python queueInfoScraper.py
 
+#use the updated queue info and write it into MySQL database
+python writeQueueInfoMYSQL.py
+
 #get data from panda and write to influxdb
 python getJobStatsFlat.py
 
