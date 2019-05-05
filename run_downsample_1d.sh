e8d3f2ba35ba1c5fd3afcf2770bc0536dcd9ceea
Eric Schanet <eric.schanet@cern.ch>
2019-05-05 18:05:05 +0200

    Downsampling scripts for acrontab

---
+++ run_downsample_1d.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#need python 2.7
+export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
+source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh
+lsetup python
+
+#execute script
+cd /afs/cern.ch/user/e/eschanet/qualitask/
+
+#new manual downsampling
+python downsample.y -average 1d
