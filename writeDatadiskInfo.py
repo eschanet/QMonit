38e89b1d835ef7193292526d2b03cbee244be42a
Eric Schanet <eric.schanet@cern.ch>
2019-06-12 17:15:13 +0200

    Some more changes to DAOD dashboard data

--- writeDatadiskInfo.py
+++ writeDatadiskInfo.py
@@ -17,6 +17,9 @@ logger = logger.getChild("mephisto")
 from influxdb import InfluxDBClient
 import Client
 
+from influxdb import InfluxDBClient
+import mysql.connector
+
 config = ConfigParser.ConfigParser()
 config.read("config.cfg")
 
@@ -24,15 +27,11 @@ password = config.get("credentials", "password")
 username = config.get("credentials", "username")
 database = config.get("credentials", "database")
 
-baseURL = 'http://pandaserver.cern.ch:25080/server/panda'
-
-url_cloudJobs  = baseURL + '/getJobStatistics'
-url_siteJobs   = baseURL + '/getJobStatisticsPerSiteResource'
-url_bamboo     = baseURL + '/getJobStatisticsForBamboo'
-
-bigpandaURL = 'https://bigpanda.cern.ch/dash/production/?cloudview=world&json'
-
-TIMEOUT = 20
+def customfloat(f):
+    if f:
+        return float(f)
+    else:
+        return 0.0
 
 with open('pandaqueue_scraped.json') as pandaqueue:
     panda_queues = json.load(pandaqueue)
@@ -49,108 +48,113 @@ with open('ddm_scraped.json') as ddmresource:
 with open('daods_datadisk.json') as datadisks:
     datadisk_info = json.load(datadisks)
 
-err, siteResourceStats = Client.getJobStatisticsPerSiteResource()
-
 client = InfluxDBClient('dbod-eschanet.cern.ch', 8080, username, password, "monit_jobs", True, False)
 
 points_list = []
+
+reader = mysql.connector.connect(user='monit', password=password, host='dbod-sql-graf.cern.ch', port=5501 ,database='monit_jobs')
+read_cursor = reader.cursor()
+
+logger.info('Getting existing data.')
+read_cursor.execute("select panda_queue, resource, avg1h_running_jobs, avg6h_running_jobs, avg12h_running_jobs, avg24h_running_jobs, avg7d_running_jobs, avg30d_running_jobs from jobs")
 
 # Explicitly set timestamp in InfluxDB point. Avoids having multiple entries per 10 minute interval (can happen sometimes with acron)
 epoch = datetime.utcfromtimestamp(0)
 def unix_time_nanos(dt):
     return (dt - epoch).total_seconds() * 1e9
 
-current_time = datetime.utcnow()
-current_time = current_time - timedelta(minutes=current_time.minute % 10,
-                             seconds=current_time.second,
-                             microseconds=current_time.microsecond)
-
+current_time = datetime.utcnow().replace(microsecond=0,second=0,minute=0)
 unix = int(unix_time_nanos(current_time))
 
-for site, site_result in siteResourceStats.iteritems():
+for (panda_queue, resource, avg1h_running_jobs, avg6h_running_jobs, avg12h_running_jobs, avg24h_running_jobs, avg7d_running_jobs, avg30d_running_jobs) in read_cursor:
+    try:
+        nickname = panda_resources[panda_queue] #do the mapping to actual panda queue nicknames
+    except:
+        logger.warning("Does not exist: queue: %s    Resource: %s" %(panda_queue, resource))
+        continue
 
-    for core, value in site_result.iteritems():
+    logger.debug("Queue: %s    Resource: %s" %(panda_queue, resource))
 
-        for job_status in value.keys():
 
-            # simple hack to protect against duplicate entries
-            # each site-core combination will have its unique **hash**
-            m = hashlib.md5()
-            m.update(site + core + job_status)
-            time = unix + int(str(int(m.hexdigest(), 16))[0:9])
+    # simple hack to protect against duplicate entries
+    # each site-core combination will have its unique **hash**
+    m = hashlib.md5()
+    m.update(panda_queue + resource + "running")
+    time = unix + int(str(int(m.hexdigest(), 16))[0:9])
 
-            if not site in panda_resources:
-                print("ERROR  -  Site %s not in panda resources"%site)
-                continue
+    if not panda_queue in panda_resources:
+        print("ERROR  -  Site %s not in panda resources"%panda_queue)
+        continue
 
-            queue = panda_resources[site]
+    if not panda_queue in panda_queues:
+        print("ERROR  -  Queue %s not in panda queues"%panda_queue)
+        continue
 
-            if not queue in panda_queues:
-                print("ERROR  -  Queue %s not in panda queues"%queue)
-                continue
+    atlas_site = panda_queues.get(panda_queue,{}).get("atlas_site","None")
+    type = panda_queues.get(panda_queue,{}).get("type","None")
+    cloud = panda_queues.get(panda_queue,{}).get("cloud","None")
+    site_state = panda_queues.get(panda_queue,{}).get("status","None")
+    tier = panda_queues.get(panda_queue,{}).get("tier","None")
 
-            atlas_site = panda_queues.get(queue,{}).get("atlas_site","None")
-            type = panda_queues.get(queue,{}).get("type","None")
-            cloud = panda_queues.get(queue,{}).get("cloud","None")
-            site_state = panda_queues.get(queue,{}).get("status","None")
-            tier = panda_queues.get(queue,{}).get("tier","None")
+    #Resource factor
+    if "MCORE" in resource:
+        if panda_queues[panda_queue]["corecount"]:
+            resource_factor = float(panda_queues[panda_queue]["corecount"])
+        else:
+            resource_factor = 8.0
+    else:
+        resource_factor = 1.0
 
-            #Resource factor
-            if "MCORE" in core:
-                if panda_queues[queue]["corecount"]:
-                    resource_factor = float(panda_queues[queue]["corecount"])
-                else:
-                    resource_factor = 8.0
-            else:
-                resource_factor = 1.0
+    ddm_names = panda_queues.get(panda_queue,{}).get("ddm","None").split(",")
+    datadisk_names = [d for d in ddm_names if "DATADISK" in d]
 
-            ddm_names = panda_queues.get(queue,{}).get("ddm","None").split(",")
-            datadisk_names = [d for d in ddm_names if "DATADISK" in d]
+    if len(datadisk_names) > 1:
+        logger.warning("Got more than one datadisk for: %s, %s" % (atlas_site, datadisk_names))
 
-            if len(datadisk_names) > 1:
-                logger.warning("Got more than one datadisk for: %s, %s" % (atlas_site, datadisk_names))
+    try:
+        datadisk_name = datadisk_names[0]
+        datadisk_size = datadisk_info[datadisk_name]["bytes"]/(1e9)
+        datadisk_files = datadisk_info[datadisk_name]["files"]
+    except:
+        logger.warning("Datadisk not found for: %s, %s" % (atlas_site, datadisk_names))
+        datadisk_name = "NONE"
+        datadisk_size = 0.0
+        datadisk_files = 0
 
-            try:
-                datadisk_name = datadisk_names[0]
-                datadisk_size = datadisk_info[datadisk_name]["bytes"]/(1e9)
-                datadisk_files = datadisk_info[datadisk_name]["files"]
-            except:
-                logger.warning("Datadisk not found for: %s, %s" % (atlas_site, datadisk_names))
-                datadisk_name = "NONE"
-                datadisk_size = 0.0
-                datadisk_files = 0
+    tags = {
+        "atlas_site": atlas_site,
+        "panda_queue" : panda_queue,
+        "resource" : resource,
+        "datadisk_name" : datadisk_name,
+        "type" : type,
+        "cloud" : cloud,
+        "site_state" : site_state,
+        "job_status" : "running",
+        "tier" : tier,
+    }
 
-            n_jobs = value[job_status]
+    #give some useful default values
+    for key in tags:
+        if tags[key] == "":
+            tags[key] = "No value"
 
-            tags = {
-                "atlas_site": atlas_site,
-                "panda_queue" : site,
-                "resource" : core,
-                "datadisk_name" : datadisk_name,
-                "type" : type,
-                "cloud" : cloud,
-                "site_state" : site_state,
-                "job_status" : job_status,
-                "tier" : tier,
-            }
+    json_body = {   "measurement": "datadisks",
+                    "tags": tags,
+                    "time" : time,
+                    "fields" : {
+                        "datadisk_files" : datadisk_files,
+                        "datadisk_occupied_gb" : datadisk_size,
+                        "avg1h_running_jobs":customfloat(avg1h_running_jobs),
+                        "avg6h_running_jobs":customfloat(avg6h_running_jobs),
+                        "avg12h_running_jobs":customfloat(avg12h_running_jobs),
+                        "avg24h_running_jobs":customfloat(avg24h_running_jobs),
+                        "avg7d_running_jobs":customfloat(avg7d_running_jobs),
+                        "avg30d_running_jobs":customfloat(avg30d_running_jobs),
+                        "resource_factor" : resource_factor
+                    }
+                }
 
-            #give some useful default values
-            for key in tags:
-                if tags[key] == "":
-                    tags[key] = "No value"
-
-            json_body = {   "measurement": "datadisks",
-                            "tags": tags,
-                            "time" : time,
-                            "fields" : {
-                                "datadisk_files" : datadisk_files,
-                                "datadisk_occupied_gb" : datadisk_size,
-                                "jobs" : n_jobs,
-                                "resource_factor" : resource_factor
-                            }
-                        }
-
-            points_list.append(json_body)
+    points_list.append(json_body)
 
 
 client.write_points(points=points_list, retention_policy="1h",time_precision="n")
