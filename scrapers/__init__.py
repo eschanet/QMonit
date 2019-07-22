09d34914b23d58e14ea25f468d7056813c3d7d13
Eric Schanet <eric.schanet@cern.ch>
2019-07-22 18:23:49 +0200

    Blueprint for scraper class

---
+++ scrapers/__init__.py
@@ -0,0 +1,32 @@
+import os
+
+import abc
+
+class Scraper(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def __init__(self, *args, **kwargs):
+        """Initializing the scraper object."""
+
+    @classmethod
+    def create_dir(path):
+        """Create directory with path.
+
+        :param path: directory path"""
+
+        try:
+            os.makedirs(path)
+        except OSError as error:
+            if error.errno != 17:
+                raise
+
+
+    @abc.abstractmethod
+    def write(self, data, path, *args, **kwargs):
+        """Writes data to path.
+
+        :param data: input data
+        :param path: output path
+        :param args: additional arguments that are passed to the class
+        :param kwargs: additional arguments that are passed to the class"""
