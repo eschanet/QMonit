17ce830a9d68fa57222fa5762c06a65b7956312d
Eric Schanet <eric.schanet@cern.ch>
2019-07-29 18:02:47 +0200

    This needs a lot more work still.
    Putting this in a new branch now, it's more work than expected
    (honestly, what did I expect...?)

--- run_scrapers.py
+++ run_scrapers.py
@@ -18,15 +18,19 @@ args = parser.parse_args()
 
 def run():
 
+    agis = scrapers.AGIS()
+    rebus = scrapers.REBUS()
+
     if args.interval == '10m':
         # Now run all the scrapers that should run in 10min intervals
-        agis = scrapers.AGIS()
         raw_data = agis.download(url="http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=schedconf.all")
         json_data = agis.convert(data=raw_data,pq_field="panda_resource")
         saved = agis.save(file="PQ_names_map.json",data=json_data)
 
     elif args.interval == '1h':
         # Run all the scrapers that only need to be run once per hour (because they don't change too often)
+        raw_site_data = agis.download(url="http://atlas-agis-api.cern.ch/request/site/query/list/?json&")
+        raw_ddm_data = agis.download(url="http://atlas-agis-api.cern.ch/request/ddmendpoint/query/list/?json&")
 
     else:
         # Nothing to do otherwise
--- scrapers/scraper_classes.py
+++ scrapers/scraper_classes.py
@@ -23,3 +23,24 @@ class AGIS(Scraper):
             json_data[d[pq_field]] = d
 
         return json_data
+
+
+class REBUS(Scraper):
+
+    def __init__(self, *args, **kwargs):
+         super(REBUS, self).__init__(*args, **kwargs)
+
+    def convert(self, data, pq_field="panda_queue", should_be_ordered_by="panda_queue", *args, **kwargs):
+        """Convert the REBUS data to the desired format of being ordered by Panda queues
+
+        :param data: data to be converted in the desired format"""
+
+        # We need a map between PQ names and the names that are used elsewhere. Weirdly enough, there are different variationsself.
+        pqs = pq_map.PQ_names_map()
+
+        json_data={}
+
+        for key,d in data.items():
+            json_data[d[pq_field]] = d
+
+        return json_data
