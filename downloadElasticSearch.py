c9900f6581aceb345caa48b7bf6042a069438ad1
Eric Schanet <eric.schanet@cern.ch>
2019-07-31 17:19:14 +0200

    HS06 benchmark numbers are moving averages now

--- downloadElasticSearch.py
+++ downloadElasticSearch.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import urllib
 import json, os
+import datetime
 from pprint import pprint
 from commonHelpers import fileHelpers as fh
 
@@ -49,7 +50,7 @@ def run():
                         "field": "metadata.PanDAQueue",
                         "inner_hits": {
                             "name": "most_recent",
-                            "size": 1,
+                            "size": 50,
                             "sort": [{"timestamp": "desc"}]
                         }
                     }
@@ -58,11 +59,30 @@ def run():
 
     for hit in res['hits']['hits']:
         # pprint(hit)
+
+        # get the PQ
         pq = hit.get('_source',{}).get('metadata',{}).get('PanDAQueue',None)
-        if pq:
-            latest_list = hit.get('inner_hits',{}).get('most_recent',{}).get('hits',{}).get('hits',[])
-            if len(latest_list) > 0:
-                results[pq] = latest_list[0].get('_source',{}).get('profiles',{}).get('fastBmk',{}).get('value',0.0)
+        if not pq:
+            continue
+
+        # get the list of all benchmark results
+        latest_list = hit.get('inner_hits',{}).get('most_recent',{}).get('hits',{}).get('hits',[])
+        if len(latest_list)==0:
+            continue
+
+        # get the average of the latest benchmark results.
+        # Only results not older than 7d, and a maximum of 50 results (whichever value is hit first).
+        # If we have no values more recent than 7d, simply use the last available one (that PQ is probably not online anymore anyway)
+        values=[]
+        for d in latest_list:
+            date = datetime.datetime.strptime( d.get('_source',{}).get('timestamp',""), '%Y-%m-%dT%H:%M:%SZ')
+            if (date < (datetime.datetime.now() - datetime.timedelta(days=7))) or len(values) < 5:
+                #we are within the last 7 days and haven't got 5 days outside of 7d yet
+                values.append(d)
+
+        to_average = [i.get('_source',{}).get('profiles',{}).get('fastBmk',{}).get('value',0.0) for i in values]
+        results[pq] = float(sum(to_average))/len(to_average)
+        # print(len(to_average))
 
     # pprint(results)
     saved = fh.save_json_to_file("benchmarks_elasticsearch_scraped.json",results)
