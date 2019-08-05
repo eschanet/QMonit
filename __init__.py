d8cd22cfe76a9817a0109a203cf478590e0afa2f
Eric Schanet <eschanet@lxplus725.cern.ch>
2019-08-05 17:51:45 +0200

    Some initial setup

--- __init__.py
+++ __init__.py
@@ -1,4 +1,12 @@
+# should use at least python 2.7
+import sys
+assert sys.version_info == (2,7), "QMonit needs python 2.7 - if you are in an slc environment consider using 'lsetup python'"
 
-from maps import Map
-from scrapers import Scraper
+# user shouldn't use ROOT5
+from .commonHelpers.logger import logger
+logger = logger.getChild(__name__)
+
+# import some classes into the global namespace
+from .maps import Map
+from .scrapers import Scraper
 # from writers import Writer
