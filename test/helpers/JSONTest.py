e6675b585cadd2c807268b77b1bd4e6e01eb042b
Eric Schanet <eschanet@lxplus725.cern.ch>
2019-08-05 17:53:54 +0200

    Helpers for the unit tests

---
+++ test/helpers/JSONTest.py
@@ -0,0 +1,25 @@
+import unittest
+import itertools
+import os
+import shutil
+
+from ...commonHelpers.logger import logger
+logger = logger.getChild(__name__)
+
+class JSONTest(unittest.TestCase):
+
+    # this should work, thanks: https://stackoverflow.com/a/25851972
+    @classmethod
+    def ordered(obj):
+        if isinstance(obj, dict):
+            return sorted((k, ordered(v)) for k, v in obj.items())
+        if isinstance(obj, list):
+            return sorted(ordered(x) for x in obj)
+        else:
+            return obj
+
+    def assertSame(self, a, b):
+        """
+        Asserts if a and b are the same
+        """
+        return self.assertEqual(ordered(a),ordered(b))
