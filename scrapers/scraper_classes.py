70db75d0e2e7d64f69b9068072c5bafb74d30de1
Eric Schanet <eric.schanet@cern.ch>
2019-08-05 16:27:49 +0200

    Renaming some arguments in scraper class

--- scrapers/scraper_classes.py
+++ scrapers/scraper_classes.py
@@ -9,7 +9,7 @@ class AGIS(Scraper):
     def __init__(self, *args, **kwargs):
          super(AGIS, self).__init__(*args, **kwargs)
 
-    def convert(self, data, pq_field="panda_queue", should_be_ordered_by="panda_queue", *args, **kwargs):
+    def convert(self, data, sort_field="panda_queue", should_be_sorted_by="panda_queue", *args, **kwargs):
         """Convert the AGIS data to the desired format of being ordered by Panda queues
 
         :param data: data to be converted in the desired format"""
@@ -17,7 +17,7 @@ class AGIS(Scraper):
         json_data={}
 
         for key,d in data.items():
-            json_data[d[pq_field]] = d
+            json_data[d[sort_field]] = d
 
         return json_data
 
@@ -27,7 +27,7 @@ class REBUS(Scraper):
     def __init__(self, *args, **kwargs):
          super(REBUS, self).__init__(*args, **kwargs)
 
-    def convert(self, data, pq_field="panda_queue", should_be_ordered_by="panda_queue", *args, **kwargs):
+    def convert(self, data, sort_field="panda_queue", should_be_ordered_by="panda_queue", *args, **kwargs):
         """Convert the REBUS data to the desired format of being ordered by Panda queues
 
         :param data: data to be converted in the desired format"""
@@ -38,6 +38,6 @@ class REBUS(Scraper):
         json_data={}
 
         for key,d in data.items():
-            json_data[d[pq_field]] = d
+            json_data[d[sort_field]] = d
 
         return json_data
