ecda3163b46c4c8bc8d73a19b749b4637335a583
Eric Schanet <eric.schanet@cern.ch>
2019-08-06 16:16:55 +0200

    Adding some scrapers to central script

--- run_scrapers.py
+++ run_scrapers.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import argparse
 
 from scrapers.agis import AGIS
+from scrapers.rebus import REBUS
 
 from maps import PQ_names_map as pq_map
 
@@ -30,7 +31,10 @@ def run():
         agis = AGIS()
         raw_data = agis.download(url="http://atlas-agis-api.cern.ch/request/pandaqueue/query/list/?json&preset=schedconf.all")
         json_data = agis.convert(data=raw_data,sort_field="panda_resource")
-        saved = agis.save(file="data/scraped_agis_pandaqueue.json",data=json_data)
+        if agis.save(file="data/scraped_agis_pandaqueue.json",data=json_data):
+            logger.info("Scraped PQ AGIS")
+        else:
+            logger.error("Problem scraping PQ AGIS")
 
     elif args.interval == '1h':
         # Run all the scrapers that only need to be run once per hour (because they don't change too often)
@@ -39,12 +43,34 @@ def run():
         agis = AGIS()
         raw_data = agis.download(url="http://atlas-agis-api.cern.ch/request/site/query/list/?json&")
         json_data = agis.convert(data=raw_data,sort_field="name")
-        saved = agis.save(file="data/scraped_agis_sites.json",data=json_data)
+        if agis.save(file="data/scraped_agis_sites.json",data=json_data):
+            logger.info("Scraped sites AGIS")
+        else:
+            logger.error("Problem scraping sites AGIS")
 
         # Now the DDM info from AGIS
         raw_data = agis.download(url="http://atlas-agis-api.cern.ch/request/ddmendpoint/query/list/?json&")
         json_data = agis.convert(data=raw_data,sort_field="site")
-        saved = agis.save(file="data/scraped_agis_ddm.json",data=json_data)
+        if agis.save(file="data/scraped_agis_ddm.json",data=json_data):
+            logger.info("Scraped DDM AGIS")
+        else:
+            logger.error("Problem scraping DDM AGIS")
+
+        # Next up is REBUS, start with the actual federation map
+        rebus = REBUS()
+        raw_data = rebus.download(url="https://wlcg-rebus.cern.ch/apps/topology/all/json")
+        json_data = rebus.convert(data=raw_data,sort_field="Site")
+        if rebus.save(file="data/scraped_rebus_federations.json",data=json_data):
+            logger.info("Scraped federations REBUS")
+        else:
+            logger.error("Problem scraping federations REBUS")
+
+        raw_data = rebus.download(url="https://wlcg-rebus.cern.ch/apps/pledges/resources/2019/all/json")
+        json_data = rebus.convert(data=raw_data,sort_field="Federation")
+        if rebus.save(file="data/scraped_rebus_pledges.json",data=json_data):
+            logger.info("Scraped pledges REBUS")
+        else:
+            logger.error("Problem scraping pledges REBUS")
 
     else:
         # Nothing to do otherwise
