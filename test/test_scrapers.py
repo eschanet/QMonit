89e2fd4925199078c6d2fc57fd46de2a67f11f66
Eric Schanet <eric.schanet@cern.ch>
2019-08-06 13:49:49 +0200

    Testing site scraping as well

--- test/test_scrapers.py
+++ test/test_scrapers.py
@@ -14,13 +14,24 @@ class TestAGIS(JSONTest):
 
     def test_site_scraper_conversion(self):
         """
-        Test that scraping sites with the AGIS scraper works
+        Test that converting scraped AGIS sites works
+        """
+        original_data = fh.get_json_from_file('test/references/test_input_agis_site.json')
+        output_data = fh.get_json_from_file('test/references/test_output_agis_site.json')
+        agis = AGIS()
+        my_data = agis.convert(data=original_data,sort_field="name")
+        self.assertSame(output_data, my_data)
+
+    def test_queue_scraper_conversion(self):
+        """
+        Test that converting scraped AGIS panda queues works
         """
         original_data = fh.get_json_from_file('test/references/test_input_agis_pandaqueue.json')
         output_data = fh.get_json_from_file('test/references/test_output_agis_pandaqueue.json')
         agis = AGIS()
         my_data = agis.convert(data=original_data,sort_field="panda_resource")
         self.assertSame(output_data, my_data)
+
 
 if __name__ == "__main__":
 
