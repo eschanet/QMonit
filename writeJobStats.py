d7920bd85fe70d439574046f3f75af5f9cd6a70a
Eric Schanet <eric.schanet@cern.ch>
2020-01-16 13:22:21 +0100

    Getting job per PQ, resource and prodSource nowwq

--- writeJobStats.py
+++ writeJobStats.py
@@ -14,14 +14,22 @@ import hashlib
 import ConfigParser
 import argparse
 
+import logging
+from commonHelpers.logger import logger
+logger = logger.getChild("QMonit")
+
 from influxdb import InfluxDBClient
 import Client
 
 parser = argparse.ArgumentParser(description="Write job stats")
 parser.add_argument('--debug', action='store_true', help='print debug messages')
+parser.add_argument('--testDB', action='store_true', help='Upload to test DB instead of production DB')
 parser.add_argument('--skipSubmit', action='store_true', help='do not upload to DB')
 args = parser.parse_args()
 
+
+if args.debug:
+    logger.setLevel(logging.DEBUG)
 
 #do some configurations
 config = ConfigParser.ConfigParser()
@@ -31,13 +39,6 @@ config.read("config.cfg")
 password = config.get("credentials", "password")
 username = config.get("credentials", "username")
 database = config.get("credentials", "database")
-
-#define my urls
-baseURL = 'http://pandaserver.cern.ch:25080/server/panda'
-url_cloudJobs  = baseURL + '/getJobStatistics'
-url_siteJobs   = baseURL + '/getJobStatisticsPerSiteResource'
-url_bamboo     = baseURL + '/getJobStatisticsForBamboo'
-bigpandaURL = 'https://bigpanda.cern.ch/dash/production/?cloudview=world&json'
 
 def getJSON(file):
     with open(file) as f:
@@ -52,11 +53,12 @@ federations_resources = getJSON('data/scraped_rebus_federations.json')
 benchmarks_resources = getJSON('data/scraped_elasticsearch_benchmark.json')
 
 #get the actual job numbers from panda
-err, siteResourceStats = Client.getJobStatisticsPerSiteResource(10)
+err, siteResourceStats = Client.get_job_statistics_per_site_label_resource(10)
 
 #idb client instance for uploading data later on
+db_name = "monit_jobs" if not args.testDB else "test_monit_jobs"
 if not args.skipSubmit:
-    client = InfluxDBClient('dbod-eschanet.cern.ch', 8080, username, password, "monit_jobs", True, False)
+    client = InfluxDBClient('dbod-eschanet.cern.ch', 8080, username, password, db_name, True, False)
 points_list = []
 
 # Explicitly set timestamp in InfluxDB point. Avoids having multiple entries per 10 minute interval (can happen sometimes with acron)
@@ -73,129 +75,133 @@ unix = int(unix_time_nanos(current_time))
 #unique data point is characterised by pqueue, resource type and job status
 for site, site_result in siteResourceStats.iteritems():
 
-    for core, value in site_result.iteritems():
+    for prod_source, resources in site_result.iteritems():
 
-        for job_status in value.keys():
+        for core, value in resources.iteritems():
 
-            # simple hack to protect against duplicate entries
-            # each site-core combination will have its unique **hash**
-            m = hashlib.md5()
-            m.update(site + core + job_status)
-            time = unix + int(str(int(m.hexdigest(), 16))[0:9])
+            for job_status in value.keys():
 
-            if not site in panda_resources:
-                print("ERROR  -  Site %s not in panda resources"%site)
-                continue
+                # protect against duplicate entries
+                # each site-core combination will have its unique **hash**
+                m = hashlib.md5()
+                m.update(str(site) + str(prod_source) + str(core) + str(job_status))
+                time = unix + int(str(int(m.hexdigest(), 16))[0:9])
 
-            queue = panda_resources[site]
+                if not site in panda_resources:
+                    logger.error("Site %s not in panda resources"%site)
+                    continue
 
-            if not queue in panda_queues:
-                print("ERROR  -  Queue %s not in panda queues"%queue)
-                continue
+                queue = panda_resources[site]
 
-            #information taken from main AGIS json
-            atlas_site = panda_queues.get(queue,{}).get("atlas_site","None")
-            type = panda_queues.get(queue,{}).get("type","None")
-            cloud = panda_queues.get(queue,{}).get("cloud","None")
-            site_state = panda_queues.get(queue,{}).get("status","None")
-            tier = panda_queues.get(queue,{}).get("tier","None")
-            pilot_manager = panda_queues.get(queue,{}).get("pilot_manager","None")
-            pilot_version = panda_queues.get(queue,{}).get("pilot_version","None")
-            harvester = panda_queues.get(queue,{}).get("harvester","None")
-            harvester_workflow = panda_queues.get(queue,{}).get("workflow","None")
-            container_type = panda_queues.get(queue,{}).get("container_type","None")
+                if not queue in panda_queues:
+                    logger.error("Queue %s not in panda queues"%queue)
+                    continue
 
-            #information from wlcg rebus
-            federation = federations_resources.get(atlas_site,{}).get("Federation","None")
-            pledge = ""
-            for pledges in pledges_resources.get(federation,[]):
-                pledge_type = pledges.get("PledgeType","None")
-                pledge_unit = pledges.get("PledgeUnit","None")
-                pledge += "%s (%s);" % (pledge_type, pledge_unit)
-                if pledge_unit == 'HEP-SPEC06':
-                    federation_HS06_pledge = pledges.get("ATLAS",0)
-            pledge = pledge[:-1] if len(pledge)>1 else "None" #wait a minute, is this safe for empty strings?
+                #information taken from main AGIS json
+                atlas_site = panda_queues.get(queue,{}).get("atlas_site","None")
+                type = panda_queues.get(queue,{}).get("type","None")
+                cloud = panda_queues.get(queue,{}).get("cloud","None")
+                site_state = panda_queues.get(queue,{}).get("status","None")
+                tier = panda_queues.get(queue,{}).get("tier","None")
+                pilot_manager = panda_queues.get(queue,{}).get("pilot_manager","None")
+                pilot_version = panda_queues.get(queue,{}).get("pilot_version","None")
+                harvester = panda_queues.get(queue,{}).get("harvester","None")
+                harvester_workflow = panda_queues.get(queue,{}).get("workflow","None")
+                container_type = panda_queues.get(queue,{}).get("container_type","None")
 
-            #information about frontier
-            frontier_list = site_resources.get(atlas_site, {}).get("fsconf", {}).get("frontier", [])
-            if len(frontier_list) > 0:
-                frontier = frontier_list[0]
-                if len(frontier) > 0:
-                    frontier = frontier[0]
-                    frontier = frontier.split(':')[1].replace("//","")
-            else:
-                frontier = ''
+                #information from wlcg rebus
+                federation = federations_resources.get(atlas_site,{}).get("Federation","None")
+                pledge = ""
+                for pledges in pledges_resources.get(federation,[]):
+                    pledge_type = pledges.get("PledgeType","None")
+                    pledge_unit = pledges.get("PledgeUnit","None")
+                    pledge += "%s (%s);" % (pledge_type, pledge_unit)
+                    if pledge_unit == 'HEP-SPEC06':
+                        federation_HS06_pledge = pledges.get("ATLAS",0)
+                pledge = pledge[:-1] if len(pledge)>1 else "None" #wait a minute, is this safe for empty strings?
 
-            #information about nucleus
-            data_policies = site_resources.get(atlas_site,{}).get("datapolicies",[])
-            if "Nucleus" in data_policies:
-                nucleus = atlas_site
-            else:
-                nucleus = 'None'
-
-            # FTS server information
-            fts_servers = ddm_resources.get(atlas_site,{}).get("servedrestfts",{}).get("MASTER",{})
-            if len(fts_servers) > 0:
-                fts_server = fts_servers[0].split(':')[1].replace("//","")
-            else:
-                fts_server = ''
-
-            #Resource factor
-            if "MCORE" in core:
-                if panda_queues[queue]["corecount"]:
-                    resource_factor = float(panda_queues[queue]["corecount"])
+                #information about frontier
+                frontier_list = site_resources.get(atlas_site, {}).get("fsconf", {}).get("frontier", [])
+                if len(frontier_list) > 0:
+                    frontier = frontier_list[0]
+                    if len(frontier) > 0:
+                        frontier = frontier[0]
+                        frontier = frontier.split(':')[1].replace("//","")
                 else:
-                    resource_factor = 8.0
-            else:
-                resource_factor = 1.0
+                    frontier = ''
 
-            # Corepower
-            corepower = float(panda_queues.get(queue,{}).get("corepower","1.0"))
+                #information about nucleus
+                data_policies = site_resources.get(atlas_site,{}).get("datapolicies",[])
+                if "Nucleus" in data_policies:
+                    nucleus = atlas_site
+                else:
+                    nucleus = 'None'
 
-            # Benchmarked HS06
-            benchmark_corepower = float(benchmarks_resources.get(queue,{}).get("avg_value",0.0))
+                # FTS server information
+                fts_servers = ddm_resources.get(atlas_site,{}).get("servedrestfts",{}).get("MASTER",{})
+                if len(fts_servers) > 0:
+                    fts_server = fts_servers[0].split(':')[1].replace("//","")
+                else:
+                    fts_server = ''
 
-            n_jobs = value[job_status]
+                #Resource factor
+                if "MCORE" in core:
+                    if panda_queues[queue]["corecount"]:
+                        resource_factor = float(panda_queues[queue]["corecount"])
+                    else:
+                        resource_factor = 8.0
+                else:
+                    resource_factor = 1.0
 
-            tags = {
-                "atlas_site": atlas_site,
-                "panda_queue" : site,
-                "resource" : core,
-                "type" : type,
-                "cloud" : cloud,
-                "site_state" : site_state,
-                "job_status" : job_status,
-                "tier" : tier,
-                "pilot_manager" : pilot_manager,
-                "pilot_version" : pilot_version,
-                "frontier" : frontier,
-                "harvester" : harvester,
-                "workflow" : harvester_workflow,
-                "fts_server" : fts_server,
-                "nucleus" : nucleus,
-                "container_type": container_type,
-                "federation" : federation,
-                "pledge_type" : pledge,
-            }
+                # Corepower
+                corepower = float(panda_queues.get(queue,{}).get("corepower","1.0"))
 
-            #give some useful default values
-            for key in tags:
-                if tags[key] == "":
-                    tags[key] = "No value"
+                # Benchmarked HS06
+                benchmark_corepower = float(benchmarks_resources.get(queue,{}).get("avg_value",0.0))
 
-            json_body = {   "measurement": "jobs",
-                            "tags": tags,
-                            "time" : time,
-                            "fields" : {
-                                "jobs" : n_jobs,
-                                "resource_factor" : resource_factor,
-                                "corepower" : corepower,
-                                "HS06_benchmark" : benchmark_corepower,
-                                "federation_HS06_pledge" : federation_HS06_pledge,
+                n_jobs = value[job_status]
+
+                tags = {
+                    "atlas_site": atlas_site,
+                    "panda_queue" : site,
+                    "prod_source" : prod_source,
+                    "resource" : core,
+                    "type" : type,
+                    "cloud" : cloud,
+                    "site_state" : site_state,
+                    "job_status" : job_status,
+                    "tier" : tier,
+                    "pilot_manager" : pilot_manager,
+                    "pilot_version" : pilot_version,
+                    "frontier" : frontier,
+                    "harvester" : harvester,
+                    "workflow" : harvester_workflow,
+                    "fts_server" : fts_server,
+                    "nucleus" : nucleus,
+                    "container_type": container_type,
+                    "federation" : federation,
+                    "pledge_type" : pledge,
+                }
+
+                #give some useful default values
+                for key in tags:
+                    if tags[key] == "":
+                        tags[key] = "No value"
+
+                json_body = {   "measurement": "jobs",
+                                "tags": tags,
+                                "time" : time,
+                                "fields" : {
+                                    "jobs" : n_jobs,
+                                    "resource_factor" : resource_factor,
+                                    "corepower" : corepower,
+                                    "HS06_benchmark" : benchmark_corepower,
+                                    "federation_HS06_pledge" : federation_HS06_pledge,
+                                }
                             }
-                        }
 
-            points_list.append(json_body)
+                logger.debug(json_body)
+                points_list.append(json_body)
 
 if not args.skipSubmit:
     client.write_points(points=points_list, time_precision="n")
