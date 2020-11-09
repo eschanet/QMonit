1eccb5df7fdb37ff0f51f70b570a4efc3198106f
Eric Schanet <eric.schanet@cern.ch>
2020-11-09 10:32:01 +0100

    Yapf and resource_type

--- writeQueueInfoMYSQL.py
+++ writeQueueInfoMYSQL.py
@@ -4,10 +4,12 @@ from __future__ import print_function
 
 from pprint import pprint
 from collections import defaultdict
-import json,sys
+import json, sys
 
 import requests
-requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+requests.packages.urllib3.disable_warnings(
+    requests.packages.urllib3.exceptions.InsecureRequestWarning
+)
 
 import argparse
 import ConfigParser
@@ -20,12 +22,17 @@ from influxdb import InfluxDBClient
 import mysql.connector
 
 parser = argparse.ArgumentParser(description="Derived quantities writer")
-parser.add_argument('--debug', action='store_true', help='print debug messages')
-parser.add_argument('--skipSubmit', action='store_true', help='do not upload to DB')
+parser.add_argument(
+    '--debug', action='store_true', help='print debug messages'
+)
+parser.add_argument(
+    '--skipSubmit', action='store_true', help='do not upload to DB'
+)
 args = parser.parse_args()
 
 if args.debug:
     logging.getLogger("mephisto").setLevel(logging.DEBUG)
+
 
 def run():
 
@@ -38,9 +45,21 @@ def run():
 
     logger.info('Constructing MySQL connector.')
 
-    reader = mysql.connector.connect(user='monit', password=password, host='dbod-sql-graf.cern.ch', port=5501 ,database='monit_jobs')
+    reader = mysql.connector.connect(
+        user='monit',
+        password=password,
+        host='dbod-sql-graf.cern.ch',
+        port=5501,
+        database='monit_jobs'
+    )
     read_cursor = reader.cursor()
-    writer = mysql.connector.connect(user='monit', password=password, host='dbod-sql-graf.cern.ch', port=5501 ,database='monit_jobs')
+    writer = mysql.connector.connect(
+        user='monit',
+        password=password,
+        host='dbod-sql-graf.cern.ch',
+        port=5501,
+        database='monit_jobs'
+    )
     write_cursor = writer.cursor()
 
     logger.info('Getting existing data.')
@@ -55,22 +74,32 @@ def run():
     datadisk_info = getJSON('data/scraped_grafana_datadisk.json')
     federations_resources = getJSON('data/scraped_rebus_federations.json')
 
-    for (panda_queue,prod_source,resource) in read_cursor:
+    for (panda_queue, prod_source, resource) in read_cursor:
         try:
-            nickname = panda_resources[panda_queue] #do the mapping to actual panda queue nicknames
+            nickname = panda_resources[
+                panda_queue]  #do the mapping to actual panda queue nicknames
             atlas_site = panda_queues[nickname]["atlas_site"]
         except:
-            logger.warning("Does not exist: queue: %s   Prod_source: %s    Resource: %s" %(panda_queue,prod_source, resource))
+            logger.warning(
+                "Does not exist: queue: %s   Prod_source: %s    Resource: %s" %
+                (panda_queue, prod_source, resource)
+            )
             continue
 
-        logger.debug("Queue: %s    Prod_source: %s     Resource: %s" %(panda_queue,prod_source, resource))
+        logger.debug(
+            "Queue: %s    Prod_source: %s     Resource: %s" %
+            (panda_queue, prod_source, resource)
+        )
 
         atlas_site = panda_queues[nickname]["atlas_site"]
         type = panda_queues[nickname]["type"]
         cloud = panda_queues[nickname]["cloud"]
-        federation = federations_resources.get(atlas_site,{}).get("accounting_name","None")
+        federation = federations_resources.get(atlas_site, {}).get(
+            "accounting_name", "None"
+        )
         site_state = panda_queues[nickname]["status"]
         tier = panda_queues[nickname]["tier"]
+        resource_type = panda_queues[nickname].get("resource_type", "None")
 
         if "MCORE" in resource:
             if panda_queues[nickname]["corecount"]:
@@ -80,38 +109,50 @@ def run():
         else:
             resource_factor = 1.0
 
-        ddm_names = panda_queues.get(nickname,{}).get("astorages",{}).get("read_lan",[])
+        ddm_names = panda_queues.get(nickname, {}).get("astorages",
+                                                       {}).get("read_lan", [])
 
         # ddm_names = panda_queues[nickname]["ddm"].split(",")
         datadisk_names = [d for d in ddm_names if "DATADISK" in d]
 
         if len(datadisk_names) > 1:
-            logger.warning("Got more than one datadisk for: %s, %s" % (atlas_site, datadisk_names))
+            logger.warning(
+                "Got more than one datadisk for: %s, %s" %
+                (atlas_site, datadisk_names)
+            )
 
         try:
             datadisk_name = datadisk_names[0]
-            datadisk_size = datadisk_info[datadisk_name]["bytes"]/(1e9)
+            datadisk_size = datadisk_info[datadisk_name]["bytes"] / (1e9)
             datadisk_files = datadisk_info[datadisk_name]["files"]
         except:
-            logger.warning("Datadisk not found for: %s, %s" % (atlas_site, datadisk_names))
+            logger.warning(
+                "Datadisk not found for: %s, %s" %
+                (atlas_site, datadisk_names)
+            )
             datadisk_name = "NONE"
             datadisk_size = 0.0
             datadisk_files = 0
 
-        add_point = ('''INSERT INTO jobs (panda_queue, prod_source, resource) VALUES ("{panda_queue}","{prod_source}", "{resource}") ON DUPLICATE KEY UPDATE atlas_site="{atlas_site}", type="{type}", cloud="{cloud}",federation="{federation}", site_state="{site_state}", tier="{tier}",resource_factor="{resource_factor}", datadisk_name="{datadisk_name}", datadisk_occupied_gb="{datadisk_size}", datadisk_files="{datadisk_files}"'''.format(
-                    atlas_site = atlas_site,
-                    panda_queue = panda_queue,
-                    type = type,
-                    prod_source = prod_source,
-                    cloud = cloud,
-                    federation = federation,
-                    site_state = site_state,
-                    tier = tier,
-                    resource_factor=resource_factor,
-                    resource = resource,
-                    datadisk_name=datadisk_name,
-                    datadisk_size=datadisk_size,
-                    datadisk_files=datadisk_files))
+        add_point = (
+            '''INSERT INTO jobs (panda_queue, prod_source, resource) VALUES ("{panda_queue}","{prod_source}", "{resource}") ON DUPLICATE KEY UPDATE atlas_site="{atlas_site}", type="{type}", cloud="{cloud}",federation="{federation}", site_state="{site_state}", tier="{tier}",resource_factor="{resource_factor}",resource_type="{resource_type}", datadisk_name="{datadisk_name}", datadisk_occupied_gb="{datadisk_size}", datadisk_files="{datadisk_files}"'''
+            .format(
+                atlas_site=atlas_site,
+                panda_queue=panda_queue,
+                type=type,
+                prod_source=prod_source,
+                cloud=cloud,
+                federation=federation,
+                site_state=site_state,
+                tier=tier,
+                resource_factor=resource_factor,
+                resource=resource,
+                resource_type=resource_type,
+                datadisk_name=datadisk_name,
+                datadisk_size=datadisk_size,
+                datadisk_files=datadisk_files
+            )
+        )
 
         if panda_queue == 'ANALY_SiGNET':
             print(add_point)
@@ -126,5 +167,5 @@ def run():
     writer.close()
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     run()
